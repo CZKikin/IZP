bdc244f9071d9e6e36ac480089311142a6eee38b
CZKikin <krystofsadlik13@seznam.cz>
2020-09-27 11:51:52 +0200

    Added input with buffer overflow prevention and added info to readme

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Zásady projektu
 
 Nejlépe dodržujme https://suckless.org/coding_style/ tento coding style
-je to pro naše dobro.
+je to pro naše dobro.  
+Dále bych byl rád, aby si KAŽDÝ z nás udělal vlastní branch a poté co udělá
+funkční změnu vytvořil pull req. alespoň se dvěma ostatníma chábrama. Z důvodu
+čitelnosti a následné další práce s kódem.
+
 
--- sheet.c
+++ sheet.c
@@ -2,7 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char delim = ' ';
+#define FILE_DATA_LEN (4096)
+
+/* TODO: Fce budou využívat sdílene struktury, ve které
+ * jsou uloženy informace o vstupu, parametrech a datech souboru
+ * který nám byl předán */
+
+struct params{
+    char delim;
+    char command[10];
+    char arguments[3];
+    int arg_count;
+    char file_data[FILE_DATA_LEN];
+};
+
+struct params user_params = { 
+    .delim = ' '
+};
 
 void
 print_usage(){
@@ -10,26 +26,36 @@ print_usage(){
            "-d <delim> <line select> <data command>\n\r");
 }
 
+void /* To prevent buffer overflow */
+scan_input(){
+    char char_read;
+
+    for(int i=0; ((char_read = getchar()) != EOF) && (i < FILE_DATA_LEN); i++){
+      user_params.file_data[i] = char_read;
+    }
+}
+
 int
 main(int argc, char **argv){
     int opt;
     int flag_chosen = 0;
-     
+    scan_input();
+    printf("%s", user_params.file_data);
     if (argc == 1){
         print_usage();
         return 0;
     }
-
-   while ((opt = getopt(argc, argv, "d:")) != -1){
+    
+    while ((opt = getopt(argc, argv, "d:")) != -1){
         switch(opt){
             case 'd':
                 if(!flag_chosen){
                     printf("Parameter -d is %c\n\r", optarg[0]);
-                    delim = optarg[0];
+                    user_params.delim = optarg[0];
                     flag_chosen = 1;
                 } else {
-                    printf("Perimiter can be set only once!\r\n");
-                    exit(-1);
+                    printf("Perimiter can be set only once,"\
+                           " ignoring other -d flags!\r\n");
                 }
                 break;
 
