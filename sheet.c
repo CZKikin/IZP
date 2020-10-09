983b9de09b1f92f6bde0fe035b332188a76845e5
CZKikin <krystofsadlik13@seznam.cz>
2020-10-09 17:57:04 +0200

    Change scan input to read per line, implemented arow, changed argument parser to be able to catch command even if arguments are first

--- sheet.c
+++ sheet.c
@@ -4,48 +4,56 @@
 #include <string.h>
 #include <assert.h>
 
-#define FILE_DATA_LEN (4096)
+#define NUMBER_OF_COMMANDS (31)
+#define NUMBER_OF_ARGUMENTS (5)
+#define LINE_DATA_LEN (10240)
+#define ARG_LEN (100)
 
 void print_usage();
-void scan_input();
-int (*get_func_pt())();
+int scan_input();
+int (*get_func_pt(char *command))();
 int run_tests();
-int irow();
-int arow();
-int drow();
-int drows();
-int icol();
-int acol();
-int dcol();
-int dcols();
-int cset();
-int to_lower();
-int touuper();
-int roundup();
-int copy();
-int swap();
-int move();
-int csum();
-int cavg();
-int cmin();
-int cmax();
-int ccount();
-int cseq();
-int rseq();
-int rsum();
-int ravg();
-int rmin();
-int rmax();
-int rcount();
-int rows();
-int beginswith();
-int contains();
+int check_for_space(size_t size_needed);
+int count_collumns();
+int get_row_end(int cbsr);
+int (*find_command())();
+void find_arguments(int argc, char **argv);
+int irow(int last_line);
+int arow(int last_line);
+int drow(int last_line);
+int drows(int last_line);
+int icol(int last_line);
+int acol(int last_line);
+int dcol(int last_line);
+int dcols(int last_line);
+int cset(int last_line);
+int to_lower(int last_line);
+int touuper(int last_line);
+int roundup(int last_line);
+int copy(int last_line);
+int swap(int last_line);
+int move(int last_line);
+int csum(int last_line);
+int cavg(int last_line);
+int cmin(int last_line);
+int cmax(int last_line);
+int ccount(int last_line);
+int cseq(int last_line);
+int rseq(int last_line);
+int rsum(int last_line);
+int ravg(int last_line);
+int rmin(int last_line);
+int rmax(int last_line);
+int rcount(int last_line);
+int rows(int last_line);
+int beginswith(int last_line);
+int contains(int last_line);
 
 /* TODO: Fce budou využívat sdílene struktury, ve které
  * jsou uloženy informace o vstupu, parametrech a datech souboru
  * který nám byl předán */
 
-char commands[31][11] = {
+char commands[NUMBER_OF_COMMANDS][11] = {
     "irow",        /*0*/
     "arow",        /*1*/
     "drow",        /*2*/
@@ -78,7 +86,7 @@ char commands[31][11] = {
     "contains"     /*30*/
 };
 
-int (*functions[31])() = {
+int (*functions[NUMBER_OF_COMMANDS])() = {
    irow,
    arow,
    drow,
@@ -113,39 +121,211 @@ int (*functions[31])() = {
 
 struct params{
     char delim;
-    char command[11];
-    char arguments[3];
+    char arguments[NUMBER_OF_ARGUMENTS][ARG_LEN];
     int arg_count;
-    char file_data[FILE_DATA_LEN];
+    char line_data[LINE_DATA_LEN];
 };
 
 struct params user_params = {
     .delim = ' '
 };
 
-// Funkce, která vrací ukazatel na funkci
 int
-(*get_func_pt())(){
-    for (int i=0; i<31; i++){
-        if (strcmp(user_params.command, commands[i]) == 0)
+irow(int last_line){
+    (void)last_line; //pičuje compiler a ja potřebuju testovat :)
+    int selected_row = atoi(user_params.arguments[0]);
+    printf("arg:%d\n",selected_row);
+
+    //zjistim pocet : do radku R (Rxpocet :), odectu R, a budu hledat \n, za \n pridam pocet : a nakonec \n, tim mi vznikne novy radek
+
+    //index konce radku za ktery pridam radek
+    int index = get_row_end(count_collumns()*(selected_row-1));
+
+    printf("index:%d\n",index);
+
+    char start[index], end[LINE_DATA_LEN-index];
+    strncpy(start,user_params.line_data,index);
+    strcpy(end,&user_params.line_data[index]);
+
+    printf("\n%s\n", start);
+    printf("\n%s\n", end);
+
+    return 0;
+}
+int
+drow(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+drows(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+icol(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+acol(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+dcol(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+dcols(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+cset(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+to_lower(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+touuper(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+roundup(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+copy(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+swap(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+move(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+csum(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+cavg(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+cmin(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+cmax(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+ccount(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+cseq(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+rseq(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+rsum(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+ravg(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+rmin(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+rmax(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+rcount(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+rows(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+beginswith(int last_line){
+    (void)last_line;
+    return -1;
+}
+int
+contains(int last_line){
+    (void)last_line;
+    return -1;
+}
+
+int //vlozi novy radek na konec souboru
+arow(int last_line){
+    if (!last_line){
+        return 0;
+    } else {
+
+        if (check_for_space(1) != 0)
+            return -1;
+
+        sprintf(user_params.line_data, "%c", user_params.delim);
+    }
+    return 0;
+}
+
+int
+(*get_func_pt(char *command))(){
+    for (int i=0; i<NUMBER_OF_COMMANDS; i++){
+        if (strcmp(command, commands[i]) == 0)
             return functions[i];
     }
-    printf("Invalid command\r\n");
     return NULL;
 }
 
 int
-run_tests(){
-    printf("----TESTING----\r\n");
-    int (*chosen_command)();
-    scan_input();
-
-    assert(user_params.file_data[FILE_DATA_LEN] == '\0');
-
-    chosen_command = get_func_pt();
-    assert(chosen_command  != NULL);
-
+run_tests(int(*chosen_command)()){
     int result;
+
+    printf("----TESTING----\r\n");
+    result = scan_input();
+
+    assert(result == 0);
+    assert(user_params.line_data[LINE_DATA_LEN] == '\0');
+
     result = chosen_command();
     assert(result == 0);
 
@@ -159,27 +339,35 @@ print_usage(){
            "-d <delim> <line select> <data command>\n\rFor testing: -t\r\n");
 }
 
-void /* To prevent buffer overflow */
-scan_input(){
-    char char_read;
 
-    for(int i=0; ((char_read = getchar()) != EOF) && (i < FILE_DATA_LEN); i++){
-      user_params.file_data[i] = char_read;
+int /* To prevent buffer overflow */
+scan_input(){
+    char ch;
+
+    memset(user_params.line_data, 0, sizeof(user_params.line_data));
+    for(int i=0; ((ch = getchar()) != EOF && ch != '\n') && \
+            (i < LINE_DATA_LEN); i++){
+      user_params.line_data[i] = ch;
     }
+    if (ch == EOF)
+        return 1; 
+
+    return 0;
 }
 
 int
 check_for_space(size_t size_needed){     /* -1 because indexing from 0 */
-    if (user_params.file_data[FILE_DATA_LEN - (size_needed - 1)] != '\0')
-        return -1; /* There is not enough space for that operation */
+    if (user_params.line_data[LINE_DATA_LEN - (size_needed - 1)] != '\0')
+        return -1; 
     return 0;
 }
+
 int //vrati pocet sloupcu v souboru
 count_collumns(){
     int count = 0;
 
-    for(int i = 0; user_params.file_data[i] != '\n'; i++){
-        if(user_params.file_data[i] == user_params.delim)
+    for(int i = 0; user_params.line_data[i] != '\n'; i++){
+        if(user_params.line_data[i] == user_params.delim)
         {
             count++; //spocita pocet rozdělovačů
         }
@@ -193,16 +381,16 @@ get_row_end(int cbsr){
 
     int index = 0;
     int q = 0; //pocet nalezenych rozdelovacu
-    for(int i = 0; user_params.file_data[i] != '\0'; i++){
+    for(int i = 0; user_params.line_data[i] != '\0'; i++){
         if(q == cbsr){
             //vyhledani konce radku
-            for(int t = i; user_params.file_data[t] != '\n'; t++){
+            for(int t = i; user_params.line_data[t] != '\n'; t++){
                 index = t;
             }
             return index+1;
         }
 
-        if(user_params.file_data[i] == user_params.delim)
+        if(user_params.line_data[i] == user_params.delim)
         {
             q++;
         }
@@ -213,161 +401,27 @@ get_row_end(int cbsr){
 }
 
 int
-irow(){
-    int selected_row = atoi(user_params.arguments);
-    printf("arg:%d\n",selected_row);
+(*find_command())(){
+    int (*chosen_command)();
 
-    //zjistim pocet : do radku R (Rxpocet :), odectu R, a budu hledat \n, za \n pridam pocet : a nakonec \n, tim mi vznikne novy radek
-
-    //index konce radku za ktery pridam radek
-    int index = get_row_end(count_collumns()*(selected_row-1));
-
-    printf("index:%d\n",index);
-
-    char start[index], end[FILE_DATA_LEN-index];
-    strncpy(start,user_params.file_data,index);
-    strcpy(end,&user_params.file_data[index]);
-
-    printf("\n%s\n", start);
-    printf("\n%s\n", end);
-
-    return 0;
-}
-int
-drow(){
-    return -1;
-}
-int
-drows(){
-    return -1;
-}
-int
-icol(){
-    return -1;
-}
-int
-acol(){
-    return -1;
-}
-int
-dcol(){
-    return -1;
-}
-int
-dcols(){
-    return -1;
-}
-int
-cset(){
-    return -1;
-}
-int
-to_lower(){
-    return -1;
-}
-int
-touuper(){
-    return -1;
-}
-int
-roundup(){
-    return -1;
-}
-int
-copy(){
-    return -1;
-}
-int
-swap(){
-    return -1;
-}
-int
-move(){
-    return -1;
-}
-int
-csum(){
-    return -1;
-}
-int
-cavg(){
-    return -1;
-}
-int
-cmin(){
-    return -1;
-}
-int
-cmax(){
-    return -1;
-}
-int
-ccount(){
-    return -1;
-}
-int
-cseq(){
-    return -1;
-}
-int
-rseq(){
-    return -1;
-}
-int
-rsum(){
-    return -1;
-}
-int
-ravg(){
-    return -1;
-}
-int
-rmin(){
-    return -1;
-}
-int
-rmax(){
-    return -1;
-}
-int
-rcount(){
-    return -1;
-}
-int
-rows(){
-    return -1;
-}
-int
-beginswith(){
-    return -1;
-}
-int
-contains(){
-    return -1;
-}
-
-int //vlozi novy radek na konec souboru
-arow(){
-    char new_line[3] = {'\n', user_params.delim, '\n'};
-    /* Check if there is enogh space to add new line */
-    if (check_for_space(3) != 0)
-        return -1;
-
-    for (int i=0; i<FILE_DATA_LEN - 1; i++){
-        if (user_params.file_data[i] == '\0'){ /* finds the end of a string */
-
-            if (user_params.file_data[i-1] == '\n'){
-                new_line[0] = new_line[1];
-                new_line[1] = new_line[2];
-                new_line[2] = '\0';
-            }
-
+    for (int i = 0; i < NUMBER_OF_ARGUMENTS; i++){
+        if ((chosen_command = get_func_pt(user_params.arguments[i])) != NULL){
+            for (int j=i; j<NUMBER_OF_ARGUMENTS; j++)
+                strncpy(user_params.arguments[j], user_params.arguments[j + 1], ARG_LEN); 
+            return chosen_command;
+      
         }
     }
-
-    strncat(user_params.file_data, new_line, 3);
-    return 0;
+    return NULL;
 }
+
+void
+find_arguments(int argc, char **argv){
+    for (int i = optind, j = 0; i<argc; i++,j++){
+        strncpy(user_params.arguments[j], argv[i], ARG_LEN); 
+    }
+}
+
 
 int
 main(int argc, char **argv){
@@ -407,30 +461,31 @@ main(int argc, char **argv){
 
     if (optind < argc)
     {
-        int arguments_index = 0;
-        strncpy(user_params.command, argv[optind], strlen(argv[optind]));
-        for (int i = 0; i<(int)strlen(argv[optind+1]); i++){
-            user_params.arguments[arguments_index] = argv[optind+1][i];
-            arguments_index++;
-        }
+        find_arguments(argc, argv);
+
+        chosen_command = find_command();
+        if (chosen_command == NULL)
+            return -1;
 
     } else {
-        printf("Missing command or arguments\r\n");
+        printf("Missing arguments\r\n");
         return -1;
     }
 
     if (test_flag){
-        result = run_tests();
+        result = run_tests(chosen_command);
         return result;
     }
 
-    scan_input();
+    while (scan_input() != 1){
+        if ((result = chosen_command(0)) != 0)
+            return -1;
+        printf("%s\n", user_params.line_data);
+    }
 
-    if ((chosen_command = get_func_pt(user_params.command)) != NULL)
-        result = chosen_command();
+    if ((result = chosen_command(1)) != 0)
+        return -1;
+    printf("%s\n", user_params.line_data);
 
-    if (result == 0)
-        printf("%s", user_params.file_data);
-
-    return result;
+    return 0;
 }
