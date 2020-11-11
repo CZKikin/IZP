b595769f6d6da490b24e4551168d65fb48db6486
CZKikin <krystofsadlik13@seznam.cz>
2020-11-12 00:17:11 +0100

    Big file generator finished

--- big_file_generator.py
+++ big_file_generator.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 import sys
+import random
 
-if (int(sys.argv[1]) > 0):
-    with open("test_file", "w") as file:
-        file.write(int(sys.argv[1]) * "A")
-else:
-    print("Pičo...\n");
+if sys.argv[1] == "--help" or sys.argv[1] == "-h":
+    print("""Usage: script pocet_radku <soubor_se_slovama
+    """)
+    quit()
+
+w = sys.stdin.read() 
+w = w.split()
+
+if len(sys.argv) < 2:
+    print("Missing argument")
+    quit(-1)
+
+with open("test_file", "w") as file:
+    for i in range(0, int(sys.argv[1])):
+        for f in range(random.randint(0,60)):
+            file.write(random.randint(0, 3) * str(random.choice(w)) + ":")
+        file.write("\n")
